f8789b451d5cbb42a91abbc07e3ecac44857a9e2
acronix70 <kc80094@gmail.com>
2025-04-24 14:13:31 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,25 +2,14 @@ import os
 import server
 from aiohttp import web
 
-# Configuración del directorio raíz (donde están css, js, etc.)
-WEBROOT_APPS = os.path.join(os.path.dirname(os.path.realpath(__file__)), "docs")
+# HOME_APPS
+WEBROOT_APPS = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 
-# Ruta principal para servir index.html
 @server.PromptServer.instance.routes.get("/")
-async def apps_entrance(request):
+def apps_entrance(request):
     return web.FileResponse(os.path.join(WEBROOT_APPS, "index.html"))
 
-# Configurar rutas estáticas para css, js, e imágenes
-server.PromptServer.instance.routes.static("/css", path=os.path.join(WEBROOT_APPS, "css"))
-server.PromptServer.instance.routes.static("/js", path=os.path.join(WEBROOT_APPS, "js"))
-server.PromptServer.instance.routes.static("/images", path=os.path.join(WEBROOT_APPS, "images"))  # Ejemplo para imágenes
+server.PromptServer.instance.routes.static("/", 
+path=os.path.join(WEBROOT_APPS))  # Ruta para servir el archivo config.json
 
-# Rutas adicionales para otros archivos (como JSON o documentos)
-@server.PromptServer.instance.routes.get("/{filename}")
-async def serve_files(request):
-    filename = request.match_info['filename']  # Obtener el nombre del archivo solicitado
-    file_path = os.path.join(WEBROOT_APPS, filename)  # Construir la ruta completa
-    if os.path.exists(file_path):  # Verificar que el archivo existe
-        return web.FileResponse(file_path)
-    else:
-        return web.json_response({"error": "Archivo no encontrado"}, status=404)
+
