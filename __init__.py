8f76ab7eee8efc3948cb65df5b4ae2b624697c09
acronix70 <kc80094@gmail.com>
2025-04-24 11:08:08 -0600

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,15 @@
+import os
+import server
+from aiohttp import web
+
+# HOME_APPS
+WEBROOT_APPS = os.path.join(os.path.dirname(os.path.realpath(__file__)), "docs")
+
+@server.PromptServer.instance.routes.get("/docs")
+def apps_entrance(request):
+    return web.FileResponse(os.path.join(WEBROOT_APPS, "index.html"))
+
+server.PromptServer.instance.routes.static("/docs/", 
+path=os.path.join(WEBROOT_APPS))  # Ruta para servir el archivo config.json
+
+
