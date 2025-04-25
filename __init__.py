8f1b8994bd864f78fa57bf49eb482d6c6841de39
acronix70 <kc80094@gmail.com>
2025-04-25 15:00:21 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,13 +3,13 @@ import server
 from aiohttp import web
 
 # HOME_APPS
-WEBROOT_APPS = os.path.join(os.path.dirname(os.path.realpath(__file__)), "home")
+WEBROOT_APPS = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 
 @server.PromptServer.instance.routes.get("/")
 def apps_entrance(request):
     return web.FileResponse(os.path.join(WEBROOT_APPS, "index.html"))
 
-server.PromptServer.instance.routes.static("/home/", 
+server.PromptServer.instance.routes.static("/", 
 path=os.path.join(WEBROOT_APPS))  # Ruta para servir el archivo config.json
 
 
